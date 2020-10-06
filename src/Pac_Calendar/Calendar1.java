20c93674742ab45576c94380e8b76a65169f3d2b
BH <BH@BH-PC>
2020-10-06 23:45:13 +0900

    change - setting calendar form

--- src/Pac_Calendar/Calendar1.java
+++ src/Pac_Calendar/Calendar1.java
@@ -2,6 +2,12 @@ package Pac_Calendar;
 
 public class Calendar1 {
 	public static void main(String[] args) {
-		System.out.println("Making Calendar");
-	}
+		System.out.println("[Calendar] Year 2020. Month 10");
+		System.out.println("    월\t   화\t   수\t   목\t   금\t   토\t   일\t");
+		System.out.println("  \t \t \t  1\t  2\t  3\t  4\t");
+		System.out.println("  5\t  6\t  7\t  8\t  9\t 10\t 11\t");
+		System.out.println(" 12\t 13\t 14\t 15\t 16\t 17\t 18\t");
+		System.out.println(" 19\t 20\t 21\t 22\t 23\t 24\t 25\t");
+		System.out.println(" 26\t 27\t 28\t 29\t 30\t 31\t \t");
+	} 
 }
