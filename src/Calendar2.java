b45fcd37decb484e300bc462cef373e4275e1972
BH <BH@BH-PC>
2020-10-06 23:11:40 +0900

    init Calendar project

---
+++ src/Calendar2.java
@@ -0,0 +1,7 @@
+
+
+public class Calendar2 {
+	public static void main(String[] args) {
+		System.out.println("Making Calendar");
+	}
+}
---
+++ src/Pac_Calendar/Calendar1.java
@@ -0,0 +1,7 @@
+package Pac_Calendar;
+
+public class Calendar1 {
+	public static void main(String[] args) {
+		System.out.println("Making Calendar");
+	}
+}
