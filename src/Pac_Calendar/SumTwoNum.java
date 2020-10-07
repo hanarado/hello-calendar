1b008751a99341d1d122ab9f78997ea14e7fa39f
BH <BH@BH-PC>
2020-10-07 23:19:35 +0900

    init sum two digits

---
+++ src/Pac_Calendar/SumTwoNum.java
@@ -0,0 +1,22 @@
+package Pac_Calendar;
+import java.util.*;
+
+public class SumTwoNum {
+
+	public static void main(String[] args) {
+		int firstNo, secondNo;
+		int sum = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter 2 numbers!");
+		
+		firstNo = sc.nextInt();
+		secondNo = sc.nextInt();
+		
+		sum = firstNo + secondNo;
+		
+		System.out.println(firstNo + " + " + secondNo + " = " + sum);
+		
+	}
+}
