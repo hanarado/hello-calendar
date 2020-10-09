57ad4af86025c33f840ccea3b722d8c45d48d7eb
BH <BH@BH-PC>
2020-10-09 16:18:44 +0900

    - init input월 calendar생성

---
+++ src/Pac_Calendar/Calendar3.java
@@ -0,0 +1,22 @@
+/*
+월을 입력하면 해당월의 달력을 출력한다.
+달력은 모양은 1단계에서 작성한 모양으로 만든다.
+1일은 일요일로 정해도 무방하다.
+-1을 입력받기 전까지 반복 입력받는다.
+프롬프트를 출력한다.
+# my 추가
+- 서기 1년 1월 1일의 요일은 월요일
+- 윤년과 윤달 고려
+ */
+package Pac_Calendar;
+import java.util.*;
+
+public class Calendar3 {
+
+	public static void main(String[] args) {
+		System.out.println(" just test");
+		
+		
+ 	}
+		
+}
